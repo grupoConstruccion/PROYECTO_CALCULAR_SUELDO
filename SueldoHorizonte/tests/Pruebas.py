ba9668be6e753d3f0f9d3b8ce56100e30d11d7de
MadelinHuachos <143139494+MadelinHuachos@users.noreply.github.com>
2023-12-07 16:34:34 -0500

    Update Pruebas.py

--- SueldoHorizonte/tests/Pruebas.py
+++ SueldoHorizonte/tests/Pruebas.py
@@ -15,6 +15,17 @@ class TestSueldoHorizonte(unittest.TestCase):
         self.assertEqual(faltas, 2)
         self.assertEqual(tardanza, 10)
         self.assertAlmostEqual(extras, 3.0)
+        
+    def test_calcular_bonificaciones(self):
+        bonificaciones = calcular_bonificaciones(2000.0, 3.0)
+        self.assertAlmostEqual(bonificaciones, 1064.5)
 
+    def test_calcular_descuentos(self):
+        descuentos = calcular_descuentos(2000.0, 2, 10)
+        self.assertAlmostEqual(descuentos, 134.72222222222223)
+
+    def test_calcular_sueldo_a_pagar(self):
+        sueldo_neto = calcular_sueldo_a_pagar(2000.0, 1050.0, 200.0)
+        self.assertAlmostEqual(sueldo_neto, 2850.0)
 if __name__ == '__main__':
     unittest.main()
